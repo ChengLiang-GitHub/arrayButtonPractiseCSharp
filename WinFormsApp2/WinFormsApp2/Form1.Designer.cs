f2835c5210360e99bebd32f90e92603ee32e6799
cheng liang <goodgoodeat.cs08@nycu.edu.tw>
2024-06-13 12:30:50 +0800

    init ver.

---
+++ WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            showBTN = new Button();
+            pictureBox1 = new PictureBox();
+            tabControl1 = new TabControl();
+            showTP = new TabPage();
+            testTP = new TabPage();
+            testBTN = new Button();
+            testRTB = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabControl1.SuspendLayout();
+            showTP.SuspendLayout();
+            testTP.SuspendLayout();
+            SuspendLayout();
+            // 
+            // showBTN
+            // 
+            showBTN.Location = new Point(27, 19);
+            showBTN.Name = "showBTN";
+            showBTN.Size = new Size(130, 80);
+            showBTN.TabIndex = 0;
+            showBTN.Text = "show";
+            showBTN.UseVisualStyleBackColor = true;
+            showBTN.Click += showBTN_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(413, 377);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(313, 246);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(showTP);
+            tabControl1.Controls.Add(testTP);
+            tabControl1.Location = new Point(22, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1102, 675);
+            tabControl1.TabIndex = 2;
+            // 
+            // showTP
+            // 
+            showTP.Controls.Add(showBTN);
+            showTP.Controls.Add(pictureBox1);
+            showTP.Location = new Point(4, 24);
+            showTP.Name = "showTP";
+            showTP.Padding = new Padding(3);
+            showTP.Size = new Size(1094, 647);
+            showTP.TabIndex = 0;
+            showTP.Text = "show page";
+            showTP.UseVisualStyleBackColor = true;
+            // 
+            // testTP
+            // 
+            testTP.Controls.Add(testRTB);
+            testTP.Controls.Add(testBTN);
+            testTP.Location = new Point(4, 24);
+            testTP.Name = "testTP";
+            testTP.Padding = new Padding(3);
+            testTP.Size = new Size(1094, 647);
+            testTP.TabIndex = 1;
+            testTP.Text = "test page";
+            testTP.UseVisualStyleBackColor = true;
+            // 
+            // testBTN
+            // 
+            testBTN.Location = new Point(26, 32);
+            testBTN.Name = "testBTN";
+            testBTN.Size = new Size(89, 47);
+            testBTN.TabIndex = 0;
+            testBTN.Text = "test";
+            testBTN.UseVisualStyleBackColor = true;
+            testBTN.Click += testBTN_Click;
+            // 
+            // testRTB
+            // 
+            testRTB.Location = new Point(183, 30);
+            testRTB.Name = "testRTB";
+            testRTB.Size = new Size(871, 522);
+            testRTB.TabIndex = 1;
+            testRTB.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1212, 699);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabControl1.ResumeLayout(false);
+            showTP.ResumeLayout(false);
+            testTP.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button showBTN;
+        private PictureBox pictureBox1;
+        private TabControl tabControl1;
+        private TabPage showTP;
+        private TabPage testTP;
+        private RichTextBox testRTB;
+        private Button testBTN;
+    }
+}
---
+++ WinFormsApp2/WinFormsApp2/Form1.cs
@@ -0,0 +1,109 @@
+using System.Reflection;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int findMap(string path)
+        {
+            int i;
+            for (i = 0; i < g.Length; i++)
+            {
+                if (path.Contains(g[i]))
+                    return i;
+            }
+            return -1;
+        }
+
+        Dictionary<int, string> img256PixelMap = new Dictionary<int, string>();
+        Dictionary<int, string> img72PixelMap = new Dictionary<int, string>();
+        void createMap(string directoryPath, Dictionary<int, string> imgXXPixelMap)
+        {
+            if (Directory.Exists(directoryPath))
+            {
+                // Get the list of files in the directory
+                string[] allFilePath = Directory.GetFiles(directoryPath);
+
+                // Display the list of files
+
+                foreach (var aMap in allFilePath.Select((aFilePath, pathIndex) => new { animalIndex = findMap(aFilePath), aFilePath }))
+                {
+                    imgXXPixelMap.Add(aMap.animalIndex, aMap.aFilePath);
+
+                }
+                for (int i = 0; i < img256PixelMap.Count; i++)
+                {
+                    testRTB.Text += $"animal index={i} file path={imgXXPixelMap[i]} \n";
+                }
+                //foreach (string aFilePath in allFilePath)
+                //{
+                //    string[] eachFile = aFilePath.Split("\\");
+                //    foreach(string aFile in eachFile )
+                //        for(int i = 0; i < g.Length; i++)
+                //            if (aFile.Contains(g[i]))
+                //                testRTB.Text += aFile + "\n";
+                //}
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            testRTB.Text = "Files in the directory:\n";
+            createMap("..\\..\\..\\..\\img256px\\", img256PixelMap);
+            createMap("..\\..\\..\\..\\img72px\\", img72PixelMap);
+        }
+
+        string[] g = { "Mouse", "Cow", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Chicken", "Dog", "Pig" };
+        private void showBTN_Click(object sender, EventArgs e)
+        {
+            Button[] btn = new Button[12];        //建立3個button 空間
+                                                  //
+
+
+            for (int i = 0; i < btn.Length; i++)
+            {
+                btn[i] = new Button();
+                btn[i].Name = "btn" + i;                                        	//先建立名稱(name 本名)
+                btn[i].Text = (i + 1).ToString() + " : " + g[i];
+                btn[i].Location = new Point(200 + 180 * (i % 4), 50 + 100 * (i / 4));            	//指定位置
+                btn[i].Size = new Size(130, 80);
+                btn[i].Image = Image.FromFile(img72PixelMap[i]);
+                btn[i].ImageAlign = ContentAlignment.MiddleRight;
+                btn[i].TextAlign = ContentAlignment.MiddleLeft;
+                btn[i].Click += btn_Click;
+
+            }
+            showTP.Controls.AddRange(btn);
+        }
+        void btn_Click(object sender, EventArgs e)
+        {
+            string s = (sender as Button).Text;
+            //MessageBox.Show(sTrip);
+            //MessageBox.Show("" + s.ToCharArray()[0] + " " + Convert.ToInt32(s.ToCharArray()[0]));
+            string numStr="";
+
+            foreach(char c in s)
+            {
+                if (c == ':' | c == ' ')
+                    break;
+                else
+                    numStr += c;
+            }
+            int i = Convert.ToInt32(s[0]) - '0' - 1;
+            pictureBox1.Image = Image.FromFile(img256PixelMap[Int32.Parse(numStr)-1]);
+        }
+
+
+        private void testBTN_Click(object sender, EventArgs e)
+        {
+
+            testRTB.Text += Int32.Parse("100");
+        }
+    }
+}
